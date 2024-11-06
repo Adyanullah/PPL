cbc0bd4676126c699d262fd649bc491c001ddca0
Adyanullah <151485379+Adyanullah@users.noreply.github.com>
2024-11-06 15:11:35 +0700

    Initial commit

---
+++ Assets/Scripts/MainMenuScript.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void StartButton(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+        // SceneManager.LoadLevel(1);
+    }
+
+    public void ExitButton()
+    {
+        Application.Quit();
+    }
+}
